a61cf3285b8d629272e329c09f7a887f65869b3f
wesleyvlk <wesleymuniz20@gmail.com>
2022-10-14 15:31:19 -0300

    feat!: finaliza exemplo codigo dio operadores

--- dio-sintaxe-java/src/Operadores.java
+++ dio-sintaxe-java/src/Operadores.java
@@ -1,6 +1,9 @@
 import java.util.Date;
 
 public class Operadores {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         // operadores atribuicao
         String nome = "WESLEY"; // aspas duplas para string
@@ -19,12 +22,41 @@ public class Operadores {
         double multCalculos = (454 * 55) + (524 / 56);
 
         // concatenacao
-        String nomeLang = "Linguagem" + "JAVA";
+        String nomeLang = "Linguagem " + "JAVA";
         String fakeSoma = "24.2" + "25.7";
-
+        String concatenar = "1" + 1 + 1 + 1;
+        concatenar = "1" + (1 + 1 + 1);
+        
         // operador unario
         int numero = 5;
         numero = -numero;
         numero = numero * -1;
+
+        // increment decrement
+        numero++;
+        numero--;
+
+        // invert
+        boolean valorVerdadeiro = true;
+        System.out.println(!valorVerdadeiro);
+
+        // operadores logicos
+        String nome1 = "Wesley";
+        String nome2 = new String("Wesley");
+        System.out.println(nome1.equals(nome2));
+
+        int numero1 = 1;
+        int numero2 = 1;
+
+        boolean condicao1 = true;
+        boolean condicao2 = !false;
+        
+        if (condicao1 && (7 > 4)) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das duas condições é verdadeira");
+        }
+
     }
 }

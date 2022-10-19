6ccafbccf2786d1f9bf87918aac0f117f64bb300
wesleyvlk <wesleymuniz20@gmail.com>
2022-10-19 14:45:22 -0300

    feat: adiciona exemplo codigo dio metodos
    atividade de metodos java, curso Aprendendo a Sintaxe Java - dio
    desenvolvido uma simulacao de aplicacao de TV:
      - SmartTV.java // aplicacao,
      - ControlTV.java // runtime

---
+++ dio-sintaxe-java/src/metodos/ControlTV.java
@@ -0,0 +1,18 @@
+public class ControlTV {
+    public static void main(String[] args) {
+        SmartTV smartTV = new SmartTV();
+
+        // Commands Remote Control:
+        // Power_ON/OFF, Channel_Set/UP/Down, Volume_Up/Down
+        // Status_Ok/Volume
+
+        smartTV.Power_ON();
+        smartTV.Volume_Up();
+        smartTV.Volume_Up();
+        smartTV.Volume_Down();
+        smartTV.Channel_Up();
+        smartTV.Channel_Down();
+        smartTV.Channel_Set();
+
+    }
+}

936e53ff943474047cb32e0ad4900266af8eb0f3
wesleyvlk <wesleymuniz20@gmail.com>
2022-10-26 09:21:56 -0300

    feat: adiciona exemplo codigo dio loops
    iniciando aulas de estrutura de repetição e arrays
    todos os exercicios de loops while, do-while e for em java:
      - Ex1_NomeIdade.java
      - Ex2_Nota.java
      - Ex3_MaiorMedia.java
      - Ex4_ParImpar.java
      - Ex5_Tabuada.java
      - Ex6_Fatorial.java

---
+++ dio-loopsArrays-java/src/loops/Ex3_MaiorMedia.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que leia 5 números
+e informe o maior número
+e a média desses números.
+*/
+public class Ex3_MaiorMedia {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        try {
+            int numero;
+            int soma = 0;
+            int maior = 0;
+            int media = 5;
+            int count = 0;
+            do {
+                System.out.print("\n  Numero: ");
+                numero = scanner.nextInt();
+                soma += numero;
+                if (numero > maior)
+                    maior = numero;
+                count++;
+            } while (count < media);
+            media = soma / media;
+            System.out.println("\n    Media: " + media);
+            System.out.println("    Maior: " + maior + "\n");
+        } catch (java.util.InputMismatchException e1) {
+            System.out.println("\n  Digite apenas números... \n");
+        }
+    }
+}

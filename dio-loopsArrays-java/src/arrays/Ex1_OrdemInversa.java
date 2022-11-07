317f587ec68cb843ee144ca62e7a9b5dd1e73ede
wesleyvlk <wesleymuniz20@gmail.com>
2022-11-07 20:43:58 -0300

    feat: adiciona exemplo codigo dio arrays
    	new file: Ex1_OrdemInversa.java
    	new file: Ex2_Consoantes.java
    	new file: Ex3_NumerosAleatorios.java
    	new file: Ex4_ArrayMultidimensional.java

---
+++ dio-loopsArrays-java/src/arrays/Ex1_OrdemInversa.java
@@ -0,0 +1,17 @@
+package arrays;
+
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+        int[] vetor = { -5, -6, 50, 25, 4, 6 };
+
+        System.out.print("\n  Vetor[] = ");
+        for (int i = vetor.length - 1; i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println("\n");
+    }
+}

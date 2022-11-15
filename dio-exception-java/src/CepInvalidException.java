f0006dae513b1ac8f74ba878c52939a365ec54ed
wesleyvlk <wesleymuniz20@gmail.com>
2022-11-15 18:44:01 -0300

    feat: adiciona exemplo codigo dio exceptions
    Changes to be committed:
    	new file:   dio-exception-java/src/AboutMe.java
    	new file:   dio-exception-java/src/CepInvalidException.java
    	new file:   dio-exception-java/src/FormatterCepExemple.java

---
+++ dio-exception-java/src/CepInvalidException.java
@@ -0,0 +1,2 @@
+public class CepInvalidException extends Exception {
+}
---
+++ dio-exception-java/src/FormatterCepExemple.java
@@ -0,0 +1,30 @@
+import java.time.format.FormatStyle;
+import java.util.Formatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class FormatterCepExemple {
+    static String formatCep(String cep) throws CepInvalidException {
+        if (cep.length() != 8)
+            throw new CepInvalidException();
+
+        return "22.333-444";
+    }
+
+    static Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    public static void main(String[] args) {
+        try {
+            System.out.println();
+            System.out.println("Digite se CEP: [Apenas números] ");
+            String formattedCep = formatCep("22333444");
+            System.out.println(formattedCep);
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println();
+            System.out.println("CEP invalido, digite um CEP correspondente. [Apenas números] ");
+            System.out.println();
+        }
+    }
+}
